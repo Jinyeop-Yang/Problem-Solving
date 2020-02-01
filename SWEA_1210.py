27d00a2bfead8359c417efd2de798a4c670707c5
Jinyeop <wlsduq1111@naver.com>
2020-02-02 01:56:37 +0900

    20200201

--- SWEA_1210.py
+++ SWEA_1210.py
@@ -1,26 +1,26 @@
-import sys
-sys.stdin = open("input.txt", "r")
+# import sys
+# sys.stdin = open("input.txt", "r")
 
 def Solve(y, x, v):
-    v[y][x] = True
+    v[y][x] = True # 방문했다는 표시
 
-    if y == 99:
-        if MAP[y][x] == 2:
+    if y == 99: # 끝 행에 도착했을 때
+        if MAP[y][x] == 2: # 도착점이면
             return True
-        return False
+        return False # 도착점이 아닌 끝행이면
 
     
-    if x == 0:
+    if x == 0: # 1열에 있으면 오른쪽만 검사해서 1이 있으면 방향 전환
         if MAP[y][x + 1] == 1 and v[y][x + 1] == False:
             return Solve(y, x + 1, v)
-        else:
+        else: 
             return Solve(y + 1, x, v)
-    elif x == 99:
+    elif x == 99: # 끝열에 있으면 왼쪽만 검사해서 1이 있으면 방향 전환
         if MAP[y][x - 1] == 1 and v[y][x - 1] == False:
             return Solve(y, x - 1, v)
         else:
             return Solve(y + 1, x, v)            
-    else:
+    else: # 왼쪽과 오른쪽중 갈 수 있는 곳이 있으면 방향전환
         if MAP[y][x - 1] == 1 and v[y][x - 1] == False:
             return Solve(y, x - 1, v)
         elif MAP[y][x + 1] == 1 and v[y][x + 1] == False:
@@ -35,7 +35,7 @@ for _ in range(10):
     for i in range(100):
         if MAP[0][i] == 1:
             visit = [[0] * 100 for _ in range(100)]
-            if Solve(0, i, visit):
+            if Solve(0, i, visit): # 도착점에 도착이 잘 됐을 경우
                 res = i
                 break
     
--- SWEA_1211.py
+++ SWEA_1211.py
@@ -1,10 +1,14 @@
-import sys
-sys.stdin = open("input.txt", "r")
-
+# import sys
+# sys.stdin = open("input.txt", "r")
+"""
+문제 오류가 있음. 도착점이 2가 아니라 1로 되어있다.
+SWEA의 INPUT파일에서 2를 찾아볼 수 없음 -> 시간 좀 많이 날림
+SWEA_1210과 거의 동일하다.
+"""
 def Solve(y, x, v, cnt):
     v[y][x] = True
 
-    if y == 99:
+    if y == 99:    # 끝행에 도착하면 이동 횟수를 리턴
         return cnt
 
     if x == 0:
@@ -29,14 +33,14 @@ for _ in range(10):
     tc = int(input())
     MAP = [list(map(int, input().split())) for _ in range(100)]
     count = 10e10
-    idx = 100
+    idx = 0
     for i in range(100):
         if MAP[0][i] == 1:
             visit = [[0] * 100 for _ in range(100)]
             temp = Solve(0, i, visit, 0)
             if temp <= count:
-                if temp == count:
-                    if idx > i:
+                if temp == count: # 이동횟수가 같으면 x가 큰 값을 idx에 넣음
+                    if idx < i:
                         idx = i
                 else:
                     count = temp
